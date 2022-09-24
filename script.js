fd6b30a808bfce1c138177757cf407a39d3152c0
Lane Denson <lanedenson@gmail.com>
2022-09-24 08:44:41 -0500

    Generating random choice of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log('hello world!');
+//Define the three options
+const choices = ["Rock", "Paper", "Scissors"]; 
 
+//Generate a random number between 0 and 2; these correspond with the choices array above.  Assign to getComputerChoice
+let getComputerChoice = choices[Math.floor(Math.random()*3)]; 
+
+//Display the computer choice in the console
+console.log(getComputerChoice);
