e0645963589740609f16bbf9f4dbb61d60a24524
Lane Denson <lanedenson@gmail.com>
2022-09-24 15:31:34 -0500

    Group win conditions
    Grouped the computer and player win conditions using nested ORs
    rather than a bunch of if / else if.

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-function getComputerChoice() {    
+"use strict";
+
+const choices = ["Rock", "Paper", "Scissors"];
+const playerSelection = "Rock";
+
+function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
 function playRound() {
-    if (playerSelection === "Rock" && computerSelection === "Paper") {
+    if ((playerSelection === "Rock" && computerSelection === "Paper") ||
+        (playerSelection === "Scissors" && computerSelection === "Rock") ||
+        (playerSelection === "Paper" && computerSelection === "Scissors")
+    ) {
         return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return ("You win! " + playerSelection + " beats " + computerSelection + ".");
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return ("You win! " + playerSelection + " beats " + computerSelection + ".");
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if ((playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
         return ("You win! " + playerSelection + " beats " + computerSelection + ".");
     } else {
         return ("It's a tie!");
     }
 }
 
-const choices = ["Rock", "Paper", "Scissors"];
-
 let computerSelection = getComputerChoice();
-
-const playerSelection = "Rock";
 
 console.log("Computer: " + computerSelection + ". Player: " + playerSelection + ".");
 
