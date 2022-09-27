a33c452dd286192ec39cfafa1d184b7d61d30128
Lane Denson <lanedenson@gmail.com>
2022-09-26 21:33:21 -0500

    Final changes before bed.

--- script.js
+++ script.js
@@ -3,38 +3,35 @@
 const choices = ["rock", "paper", "scissors"];
 let computerScore = 0;
 let playerScore = 0;
-let getPlayerSelection;
+let playerSelectionInput;
 let playerSelection;
 
+//Generate a random number between 0-2 and the associated choice from the array
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
+/* Play a round. First, check for a tie. 
+Next, see if the computer won. If so, increase computer score by 1.
+Then, check if the player won. If so, increase player score by 1.
+Otherwise, indicate the player made an invalid selection. */
 function playRound(playerSeiection, computerSelection) {
-    if ((playerSelection === "rock" && computerSelection === "paper") ||
+    if (playerSeiection === computerSelection) {
+        return (`It's a tie! You both chose ${playerSelection}.`);
+    } else if ((playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "scissors" && computerSelection === "rock") ||
         (playerSelection === "paper" && computerSelection === "scissors")
     ) {
         computerScore++;
-        return ("You lose! " + computerSelection
-            + " beats " + playerSelection + ".");
-    } 
-    
-    else if ((playerSelection === "paper" && computerSelection === "rock") ||
+        return (`You lose! ${computerSelection} beats ${playerSelection}.`);
+    } else if ((playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
         playerScore++;
-        return ("You win! " + playerSelection
-            + " beats " + computerSelection + ".");
-    } 
-    
-    else if (playerSeiection === computerSelection) {
-        return ("It's a tie! You both chose " + playerSelection + ".");
-    } 
-    
-    else {
-        return ("Please choose rock, paper, or scissors.");
+        return (`You win! ${playerSelection} beats ${computerSelection}.`);
+    } else {
+        return (`Please choose rock, paper, or scissors.`);
     }
 }
 
@@ -42,11 +39,10 @@ function game() {
     for (let i = 0; i < 5; i++) {
         getComputerChoice();
         let computerSelection = getComputerChoice().toLowerCase();
-        getPlayerSelection = prompt("Rock, Paper, or Scissors?");
-        playerSelection = getPlayerSelection.toLowerCase();
+        playerSelectionInput = prompt("Rock, Paper, or Scissors?");
+        playerSelection = playerSelectionInput.toLowerCase();
         console.log(playRound(playerSelection, computerSelection));
-        console.log("Computer score: " + computerScore + ". Player score: "
-            + playerScore + ".");
+        console.log(`Computer: ${computerScore}. Player: ${playerScore}.`);
     }
 }
 
